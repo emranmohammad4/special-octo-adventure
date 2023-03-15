a7ec1d1eb1efac2ab3ccf0843ba8b53ca18a4419
emranmohammad4 <emranmohammad4@gmail.com>
2023-03-15 18:06:10 -0400

    nacl rules

---
+++ firewalls.tf
@@ -0,0 +1,30 @@
+#NACL
+resource "aws_network_acl" "sml-nacl" {
+  vpc_id = aws_vpc.sml.id
+
+  tags = {
+    Name = "sml.nacl"
+  }
+}
+#NACL RULES -inbound
+resource "aws_network_acl_rule" "sml-ingress" {
+  network_acl_id = aws_network_acl.sml-nacl.id
+  rule_number    = 100
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 0
+  to_port        = 65535
+}
+#NACL RULES -outbound
+resource "aws_network_acl_rule" "sml-egress" {
+  network_acl_id = aws_network_acl.sml-nacl.id
+  rule_number    = 100
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 0
+  to_port        = 65535
+}
