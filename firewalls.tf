709b923a34bb4a3e6196b592357e51b0fabf44df
emranmohammad4 <emranmohammad4@gmail.com>
2023-03-16 12:11:56 -0400

    security group rules added

--- firewalls.tf
+++ firewalls.tf
@@ -43,3 +43,28 @@ resource "aws_security_group" "sml-sg" {
     Name = "sml-sg"
   }
 }
+# security group and rules 
+resource "aws_security_group_rule" "sml-ssh-rule" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sml-sg.id
+}
+resource "aws_security_group_rule" "sml-http-rule" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sml-sg.id
+}
+resource "aws_security_group_rule" "sml-outbound-rule" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sml-sg.id
+}
